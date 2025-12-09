4c865d74058247809dfe0d6600d75a9655516a3c
alex wang <anw2067@nyu.edu>
2025-12-09 11:19:19 -0500

    5e-4 LR

--- train/submit_experiments.sh
+++ train/submit_experiments.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # 12/08/2025 run with new metrics
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs.yaml 4
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs-goal2d5050.yaml 4
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs-goaldraw.yaml 4
-# maybe an extra, without any new model behaviour
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs-goal2d5050.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs-goaldraw.yaml 4
+# # maybe an extra, without any new model behaviour
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8.yaml 4
+# lol learning rate issues
+./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs.yaml 4
+./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goal2d5050.yaml 4
+./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goaldraw.yaml 4
+./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8-lr5e-4.yaml 4
 
 
 # 12/05/2025 swapping warmup to 3 for 2d5050 model as it diverged
