9f5d500e9de3a580db1c8d4cfe4ce0827f1650d8
alex wang <anw2067@nyu.edu>
2025-12-17 20:34:27 -0500

    preserveUpDown experiments with goalDraw

--- train/submit_experiments.sh
+++ train/submit_experiments.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 12/17/2025 run with preserve updown
+./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goaldraw-preserveUpDown.yaml 4
+
 # 12/08/2025 run with new metrics
 # ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs.yaml 4
 # ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr1e-3-pool_curr_obs-goal2d5050.yaml 4
@@ -7,10 +10,10 @@
 # # maybe an extra, without any new model behaviour
 # ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8.yaml 4
 # lol learning rate issues
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs.yaml 4
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goal2d5050.yaml 4
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goaldraw.yaml 4
-./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8-lr5e-4.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goal2d5050.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-proprioception-cat8-dinov3_unpool_3dposemb-proj-lr5e-4-pool_curr_obs-goaldraw.yaml 4
+# ./submit_scripts/submit_torch.sh config/torch/minimal-nomad-cat8-lr5e-4.yaml 4
 
 
 # 12/05/2025 swapping warmup to 3 for 2d5050 model as it diverged
